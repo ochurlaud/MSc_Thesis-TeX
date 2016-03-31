4fa3fefd5132d74bebcafc19125797445704b206
Olivier CHURLAUD <olivier@churlaud.com>
2016-03-31 18:38:56 +0200

    theory ok

--- scripts/kick.py
+++ scripts/kick.py
@@ -3,31 +3,21 @@
 import sys
 sys.path.append("/home/olivier/Projets/boulot/search_kicks")
 
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 import search_kicks.core as skcore
 
-if len(sys.argv) > 1 and str(sys.argv[1]) == "xkcd":
-    xkcd = True
-else:
-    xkcd = False
-
-if not xkcd:
-    import seaborn as sns
-    sns.set_style("whitegrid")
-
-kick_t = 11.
+kick_t = 18.93
 x, t = skcore.build_sine(kick_t, 6.2, [1, 4])
 
 kick_x = x[np.argmin(abs(t-kick_t))]
 
-if xkcd:
-    plt.xkcd()
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
 plt.plot(t, x)
+plt.grid()
 plt.xlabel("Phase [rad]")
 plt.ylabel("Distance to the ideal orbit")
 #plt.title("Example of a kick in the orbit")
@@ -36,8 +26,6 @@ ax.annotate('Kick', xy=(kick_t, kick_x), xytext=(13., -1.),
             arrowprops=dict(facecolor='black', shrink=0.05),
             )
 plt.ylim(-1.3, 1.3)
-if xkcd:
-    plt.savefig("../img/xkcd_kick.pdf")
-else:
-    plt.savefig("../img/kick.pdf")
+
+plt.savefig("../img/kick.pdf")
 plt.show()
