a6ef56f0317e913e0269cb976d6f7eefd64a7422
Olivier CHURLAUD <olivier.churlaud@helmholtz-berlin.de>
2016-06-15 14:51:30 +0200

    update pick

--- scripts/kick.py
+++ scripts/kick.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import sys
-sys.path.append("/home/olivier/Projets/boulot/search_kicks")
-
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 import search_kicks.core as skcore
+
+sns.set_style("ticks")
 
 kick_t = 18.93
 x, t = skcore.build_sine(kick_t, 6.2, [1, 4])
@@ -19,14 +20,15 @@ ax = fig.add_subplot(1, 1, 1)
 
 plt.plot(t, x)
 plt.grid()
-plt.xlabel("Phase [rad]")
-plt.ylabel("Distance to the ideal orbit")
+plt.xlabel(r"Phase [rad]")
+plt.ylabel(r"Distance to the ideal orbit")
 #plt.title("Example of a kick in the orbit")
 
-ax.annotate('Kick', xy=(kick_t, kick_x), xytext=(13., -1.),
+ax.annotate(r'Kick', xy=(kick_t, kick_x), xytext=(13., -1.),
             arrowprops=dict(facecolor='black', shrink=0.05),
             )
 plt.ylim(-1.3, 1.3)
+sns.despine()
 
 plt.savefig("../img/kick.pdf")
 plt.show()
