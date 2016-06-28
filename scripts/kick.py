fbc39dc345be977a060b57d42e720d3abc61734f
Olivier CHURLAUD <olivier.churlaud@helmholtz-berlin.de>
2016-06-28 19:15:01 +0200

    work

--- scripts/kick.py
+++ scripts/kick.py
@@ -9,8 +9,8 @@ import search_kicks.core as skcore
 
 sns.set_style("ticks")
 
-kick_t = 18.93
-x, t = skcore.build_sine(kick_t, 6.2, [1, 4])
+kick_t = 16.74
+x, t = skcore.build_sine(kick_t, 6.4, [1, 4])
 
 kick_x = x[np.argmin(abs(t-kick_t))]
 
@@ -24,10 +24,11 @@ plt.xlabel(r"Phase [rad]")
 plt.ylabel(r"Distance to the ideal orbit")
 #plt.title("Example of a kick in the orbit")
 
-ax.annotate(r'Kick', xy=(kick_t, kick_x), xytext=(13., -1.),
+ax.annotate(r'Kick', xy=(kick_t, kick_x), xytext=(11., -.5),
             arrowprops=dict(facecolor='black', shrink=0.05),
             )
 plt.ylim(-1.3, 1.3)
+plt.xlim(-0, 6.4*2*np.pi)
 sns.despine()
 
 plt.savefig("../img/kick.pdf")
